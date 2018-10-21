429b804d03872638b0498368147d805135a17be4
Patrick Deloulay <pdeloulay@users.noreply.github.com>
2018-10-21 09:04:45 +0800

    change var name

--- 1n2.go
+++ 1n2.go
@@ -2,14 +2,16 @@ package main
 
 // Best import
 import (
+	"example/stringutil"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 )
 
-// PageVariables super stuff
-type PageVariables struct {
+// PageContext super stuff
+type PageContext struct {
 	Date string
 	Time string
 }
@@ -19,28 +21,30 @@ type MatchDay struct {
 	Name string
 }
 
+// Main
 func main() {
 	http.HandleFunc("/", HomePage)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
-
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
 // HomePage Handler to MVP Page
 func HomePage(w http.ResponseWriter, r *http.Request) {
 
-	now := time.Now()              // find the time right now
-	HomePageVars := PageVariables{ //store the date and time in a struct
-		Date: now.Format("02-JAN-2018"),
-		Time: now.Format("12:04"),
+	now := time.Now()               // find the time right now
+	homePageContext := PageContext{ //store the date and time in a struct
+		Date: now.Format("MM DDDD"),
+		Time: now.Format("hh:mm"),
 	}
+
+	fmt.Println(stringutil.Reverse("Hello"))
 
 	t, err := template.ParseFiles("1n2.gohtml") //parse the html file 1n2.html
 	if err != nil {                             // if there is an error
 		log.Print("template parsing error: ", err) // log it
 	}
-	err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
-	if err != nil {                  // if there is an error
+	err = t.Execute(w, homePageContext) //execute the template and pass it the HomePageVars struct to fill in the gaps
+	if err != nil {                     // if there is an error
 		log.Print("template executing error: ", err) //log it
 	}
 }
